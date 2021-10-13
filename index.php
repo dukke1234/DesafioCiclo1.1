193ee93912ad52a1d5da2d1901e2a67be145186f
unknown <matt.ea@hotmail.com>
2021-10-13 17:09:05 -0300

    add desafio 1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Desafio 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../Aula 1/css/style.css">
+  <title>Desafio 1</title>
+</head>
+
+<body>
+  <!-- TOP SITE  -->
+  <header id="header-top">
+    <nav id=menu>
+      <a class="item_menulogo">LOGO</a>
+      <a class="item_menua" href="#">Sobre</a>
+      <a class="item_menua" href="#">Contato</a>
+      <a class="item_menub" href="#">HOME</a>
+    </nav>
+  </header>
+  <!-- MEIO SITE  -->
+  <main>
+    <div class="container_row">
+      <div class="layer1">
+        <p class="areatextual">Lorem ipsum dolor sit amet consectetur adipisicing elit. sed do eiusmod tempor incidudunt ut labore et dolore magna aliquas</p>
+      </div>
+      <div class="layer2">
+      <form method="post" name="cadastro">
+        <label for="email">Digite seu e-mail</label>
+        <input type="text" required placeholder="e-mail" class="txt" id="email" name="email" value="">
+        <input type="submit" class="eviarbotao" name="Registrar e-mail" value="ENVIAR" >
+        </form>
+      </div>
+    </div>
+  </main>
+  <!-- Roda Pe Site -->
+  <div class="footer">
+    <p>by TI-ACADEMY</p>
+  </div>
+
+</body>
+
+</html>
